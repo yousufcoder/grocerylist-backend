5d5ac078d1b0e6d8be9b435bcae93c3361610575
yousufcoder <md.yousufuddin93@gmail.com>
2019-04-25 08:34:27 +0530

    created database with grocerylist in server.js

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const server = express()
-const port = 3003
+const port = 3003;
+const { Client } = require('pg')
+const client = new Client()
 
-server.get('/', (req, res) => res.send('Hello World!'));
-server.get('/api/item-list', (req, res) => res.send('show the grocerylist of projectof blog'));
+
+
+server.get('/api/item-list', async(req, res)=>{
+    await client.connect()
+
+const des = await client.query('select * from grocery_items')
+await client.end()
+res.send(des.rows);
+});
+server.get('/api/item-list',(req,res)=>res.send('GroceryItems'));
 
 server.listen(port, () => console.log(`Example app listening on port ${port}!`));
