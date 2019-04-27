43f0d510e4f9b102de71678904fe76cc9fdcc050
yousufcoder <md.yousufuddin93@gmail.com>
2019-04-27 13:59:39 +0530

    added header to access origin control and also added groceryitems to view on frontend

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ const server = express()
 const port = 3003;
 const { Client } = require('pg')
 const client = new Client()
+const startcon = async()=>{
+    await client.connect()
+}
+startcon();
+
 
 
 
 server.get('/api/item-list', async(req, res)=>{
-    await client.connect()
-
 const des = await client.query('select * from grocery_items')
-await client.end()
-res.send(des.rows);
-});
-server.get('/api/item-list',(req,res)=>res.send('GroceryItems'));
 
+res.setHeader("Access-Control-Allow-Origin","*");
+res.send(des.rows);
+res.end();
+});
+//await client.end()
 server.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
