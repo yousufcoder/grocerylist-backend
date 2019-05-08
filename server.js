2e15e492c012f2b3d100e9425e2902cb73daf038
yousufcoder <md.yousufuddin93@gmail.com>
2019-05-08 10:40:55 +0530

    server.use

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ const startcon = async () => {
 startcon();
 
 
-
+server.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token");
+    next();
+});
+server.use(bodyParser.json());
 
 server.get('/api/item-list', async (req, res) => {
     const des = await client.query('select * from grocery_items')
@@ -19,20 +25,17 @@ server.get('/api/item-list', async (req, res) => {
     res.send(des.rows);
     res.end();
 });
+
 //await client.end()
-server.use(bodyParser.urlencoded({ extended: true }));
+
+
 server.post('/api/item-list', async (req, res) => {
     console.log(req.body);
-    res.send('Get post data');
+    
     const text = 'insert into grocery_items(id,itemname,quantity,amount)  values($1,$2,$3,$4) returning *'
     const values = [req.body.id, req.body.itemname, req.body.quantity, req.body.amount]
-    const des = client.query(text, values, (err, res) => {
-        if (err) {
-            console.log(err.stack)
-        } else {
-            console.log(res.rows)
-        }
-    });
+    const des = await client.query(text, values)
+    res.send(des.rows[0]); 
 });
 
     //const des = await client.query('insert into grocery_items(id,itemname,quantity,amount)  values($1,$2,$3,$4),(req.body.id,req.body.itemname,req.body.quantity,req.body.amount)');
